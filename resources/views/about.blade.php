70e37806a873645da1865b5e17705c9c3d8d2c13
Yurcell <chestayurcel@gmail.com>
2026-02-28 12:03:02 +0700

    Menambahkan fitur autentikasi login & register, dan menambahkan halaman about atau biodata

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-white leading-tight">
+            {{ __('Tentang Saya') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-white">
+                    <ul class="space-y-3">
+                        <li><strong>Nama:</strong> Chesta Yurcel Zebada</li>
+                        <li><strong>NIM:</strong> 20230140072</li>
+                        <li><strong>Program Studi:</strong> Teknologi Informasi</li>
+                        <li><strong>Hobi:</strong> Bermain Sepak Bola</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
